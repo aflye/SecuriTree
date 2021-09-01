596442939393fe58ba72e4cdc4a35309a28b0ebc
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-09-01 23:20:55 +0100

    Update on structure styling for View Entity

--- view.js
+++ view.js
@@ -14,6 +14,8 @@ window.onload = function() {
     }
 
     // get all open doors - just for practice.
+    var currentParent = "";
+    var counter = 0;
     const sql ="SELECT * FROM system_data";
     db.query(sql, function(err, result, fields){
         if(err){
@@ -25,17 +27,74 @@ window.onload = function() {
                 if(result[i].status!=null){
                     break;
                 }else{
-                    listOfChildren.push(result[i].name);
+                    if(i==0){
+                        listOfChildren.push([result[i].name, counter]);
+                        currentParent = result[i].area_id;
+                    }else{
+                        if(result[i].parent_area == currentParent){
+                            listOfChildren.push([result[i].name, counter+1]);
+                        }else{
+                            currentParent=result[i].parent_area;
+                            counter++;
+                            listOfChildren.push([result[i].name, counter]);
+                        }
+                    }
                 }
                 for(var j=i; j<result.length; j++){
                     if(result[j].parent_area == result[i].area_id && result[j].status!=null){
-                        listOfChildren.push(result[j].name);
+                        listOfChildren.push([result[j].name, result[j].status]);
                     }
                 }
                 list.push(listOfChildren);
                 listOfChildren=[];
             }
-            console.log(list);
+            console.log(list[0][0]) //Object of area
+            console.log(list[0][1]) //Object of first door
+            console.log(list[0][0][0]) //Name of Area
+            console.log(list[0][0][1]) //Counter
+            console.log(list[0][1][0]) //Name of First Door
+            console.log(list[0][1][1]) //Door Status
+
+            var element = document.getElementById("container");
+            var areaTag = document.createElement("pre");
+            var doorTag = document.createElement("pre");
+            var tab = '\t';
+
+            for(var i=0; i<list.length; i++){
+                var areaString = "";
+                var doorString = "";
+                for(l=0; l<list[i][0][1]; l++){
+                    areaString += tab;
+                    doorString += tab;
+                }
+                doorString+=tab;
+                areaString+="\\-"+list[i][0][0];
+
+                areaString = document.createTextNode(areaString);
+                // console.log("\-"+list[i][0]);
+                areaTag.appendChild(areaString)
+                element.appendChild(areaTag);
+                doorString += "|- [Doors] ";
+
+                for(var j=1; j<list[i].length; j++){
+                    doorString += list[i][j][0];
+                    if(list[i][j][1] == "open"){
+                        doorString += " (UNLOCKED)"
+                    }else{
+                        doorString +=" (LOCKED)"
+                    }
+                    doorString += ", "
+                }
+                areaTag = document.createElement("pre");
+                doorString = doorString.substring(0, doorString.length-2);
+                doorString = document.createTextNode(doorString);
+                doorTag.appendChild(doorString);
+                element.appendChild(doorTag);
+                doorTag = document.createElement("pre");
+                // console.log(doorString);
+            }
+
+            // console.log(list);
         }
     });
 }
