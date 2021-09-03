0dfdb1f7420e5a5f2f9fbaeee0e3650f0a0626a8
KeanuSwart <keanu04swart@hotmail.co.uk>
2021-09-03 11:18:00 +0100

    Progress with Entites

--- view.js
+++ view.js
@@ -13,95 +13,102 @@ window.onload = function() {
         view.innerHTML = "changed";
     }
 
-    // get all open doors - just for practice.
     var currentParent = "";
     var counter = 0;
-    const sql ="SELECT * FROM system_data";
-    db.query(sql, function(err, result, fields){
-        if(err){
-            throw err;
-        }else{
-            var list = [];
-            var listOfChildren = [];
-            for(var i=0; i<result.length; i++){
-                if(result[i].status!=null){
-                    break;
-                }else{
-                    console.log("here?");
+    var temp2
+    var rulesArr
+    getEntites();
+    
+}
 
-                    var temp = result[i];
-                    console.log("before while")
-                    while(temp.parent_area!=null){
-                        counter++;
-                        for(var x=0; x<result.length; x++){
-                            if(result[x].area_id==temp.parent_area){
-                                temp = result[x];
-                            }
+let listOfChildren=[];
+let counter=0;
+
+async function getEntites(){
+    const sql ="SELECT * FROM system_data";
+    db.query(sql, async (err, result) => {
+        if(err) throw err;
+        var list = [];
+        // var listOfChildren = [];
+        for(var i=0; i<result.length; i++){
+             if(result[i].status!=null){
+                break;
+            }else{
+                var temp = result[i];
+                while(temp.parent_area!=null){
+                    counter++;
+                    for(var x=0; x<result.length; x++){
+                        if(result[x].area_id==temp.parent_area){
+                            temp = result[x];
                         }
                     }
-                    console.log(i)
-                    listOfChildren.push([result[i].name, counter]);
-                    counter=0;
-
-
                 }
-                for(var j=i; j<result.length; j++){
-                    if(result[j].parent_area == result[i].area_id && result[j].status!=null){
-                        listOfChildren.push([result[j].name, result[j].status]);
-                    }
-                }
-                list.push(listOfChildren);
-                listOfChildren=[];
+                listOfChildren.push([result[i].name, counter]);
+                counter=0;
             }
+            console.log("atleast")
+            await accessResults(result[i], i);
+            list.push(listOfChildren);
+            listOfChildren=[];
+        }
+        var element = document.getElementById("container");
+        var areaTag = document.createElement("pre");
+        var doorTag = document.createElement("pre");
+        var tab = '\t';
 
-            console.log(list)
-            // console.log(list[0][0]) //Object of area
-            // console.log(list[0][1]) //Object of first door
-            // console.log(list[0][0][0]) //Name of Area
-            // console.log(list[0][0][1]) //Counter
-            // console.log(list[0][1][0]) //Name of First Door
-            // console.log(list[0][1][1]) //Door Status
+        for(var i=0; i<list.length; i++){
+            var areaString = "";
+            var doorString = "";
+            for(l=0; l<list[i][0][1]; l++){
+                areaString += tab;
+                doorString += tab;
+            }
+            doorString+=tab;
+             areaString+="\\-"+list[i][0][0];
 
-            var element = document.getElementById("container");
-            var areaTag = document.createElement("pre");
-            var doorTag = document.createElement("pre");
-            var tab = '\t';
+            areaString = document.createTextNode(areaString);
+            // console.log("\-"+list[i][0]);
+            areaTag.appendChild(areaString)
+            element.appendChild(areaTag);
+            doorString += "|- [Doors] ";
 
-            for(var i=0; i<list.length; i++){
-                var areaString = "";
-                var doorString = "";
-                for(l=0; l<list[i][0][1]; l++){
-                    areaString += tab;
-                    doorString += tab;
+            for(var j=1; j<list[i].length; j++){
+                doorString += list[i][j][0];
+                if(list[i][j][1] == "open"){
+                    doorString += " (UNLOCKED)"
+                }else{
+                    doorString +=" (LOCKED)"
                 }
-                doorString+=tab;
-                areaString+="\\-"+list[i][0][0];
-
-                areaString = document.createTextNode(areaString);
-                // console.log("\-"+list[i][0]);
-                areaTag.appendChild(areaString)
-                element.appendChild(areaTag);
-                doorString += "|- [Doors] ";
-
-                for(var j=1; j<list[i].length; j++){
-                    doorString += list[i][j][0];
-                    if(list[i][j][1] == "open"){
-                        doorString += " (UNLOCKED)"
-                    }else{
-                        doorString +=" (LOCKED)"
-                    }
-                    doorString += ", "
-                }
-                areaTag = document.createElement("pre");
-                doorString = doorString.substring(0, doorString.length-2);
+                doorString += ", "
+            }
+            areaTag = document.createElement("pre");
+            doorString = doorString.substring(0, doorString.length-2);
                 doorString = document.createTextNode(doorString);
                 doorTag.appendChild(doorString);
                 element.appendChild(doorTag);
                 doorTag = document.createElement("pre");
                 // console.log(doorString);
             }
+        })
+}
 
-            // console.log(list);
+function accessResults(parentResult, i){
+    console.log("welcome")
+    return new Promise(function(resolve, reject){
+        console.log("welcome2")
+        console.log(parentResult.length)
+        console.log(parentResult)
+        for(let j=i; j<parentResult.length; j++){
+            console.log("welcomeloop")
+            if(parentResult[j].parent_area == parentResult[i].area_id && parentResult[j].status!=null){
+                console.log("should ber here")
+                const sql = "SELECT * FROM access_rules WHERE door=?";
+                db.query(sql, parentResult[j].area_id, function(error, result) {
+                    if (error) reject( error);
+                    listOfChildren.push([parentResult[j].name, parentResult[j].status, result]);
+                    resolve(listOfChildren);
+                });
+            }
         }
-    });
+    })
 }
